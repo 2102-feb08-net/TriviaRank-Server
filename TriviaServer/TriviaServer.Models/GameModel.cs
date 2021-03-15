43da959ad72cb0c0798bec8837445be1a1a2589d
Josh Weaver <jweaver86@aim.com>
2021-03-15 18:32:21 -0400

    Game Repo and Interface addition

---
+++ TriviaServer/TriviaServer.Models/GameModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriviaServer.Models
+{
+    public class GameModel
+    {
+        public int Id { get; set; }
+        public string GameName { get; set; }
+        public int OwnerId { get; set; }
+        public DateTimeOffset StartDate { get; set; }
+        public DateTimeOffset EndDate { get; set; }
+        public bool GameMode { get; set; }
+        public int TotalQuestions { get; set; }
+        public bool IsPublic { get; set; }
+    }
+}
