be25735129d0f5d1099db384314dacffd428d2a8
Andrew Park <modem428@gmail.com>
2021-03-14 20:18:13 -0700

    create DAL respository folder:

---
+++ TriviaServer/TriviaServer.DAL/Repositories/PlayerRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriviaServer.DAL.Repositories
+{
+    public class PlayerRepository
+    {
+        private readonly TriviaRankContext _context;
+        public PlayerRepository(TriviaRankContext context)
+        {
+            _context = context;
+        }
+    }
+}
