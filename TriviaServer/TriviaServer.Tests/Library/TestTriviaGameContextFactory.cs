26fab781bd6b44a22fc460732b9652f46b32291f
Andrew Park <modem428@gmail.com>
2021-03-16 13:47:53 -0700

    sql lite context for tests

---
+++ TriviaServer/TriviaServer.Tests/Library/TestTriviaGameContextFactory.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TriviaServer.DAL;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace TriviaServer.Tests.Library
+{
+    public class TestTriviaGameContextFactory : IDisposable
+    {
+        private DbConnection _connection;
+        private bool _disposedValue;
+
+        private DbContextOptions<TriviaRankContext> CreateOptions()
+        {
+            return new DbContextOptionsBuilder<TriviaRankContext>()
+                .UseSqlite(_connection)
+                .Options;
+        }
+
+        public TriviaRankContext CreateContext()
+        {
+            if (_connection == null)
+            {
+                _connection = new SqliteConnection("DataSource=:memory:");
+                _connection.Open();
+
+                DbContextOptions<TriviaRankContext> options = CreateOptions();
+                using var context = new TriviaRankContext(options);
+                context.Database.EnsureCreated();
+
+                // add extra test seed data here (or, in each test method)
+            }
+
+            return new TriviaRankContext(CreateOptions());
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposedValue)
+            {
+                if (disposing)
+                {
+                    _connection.Dispose();
+                }
+
+                _disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
