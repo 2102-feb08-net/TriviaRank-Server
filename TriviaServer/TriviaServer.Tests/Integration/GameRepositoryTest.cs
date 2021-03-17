4cdd93e1bc375e839bea5e54b676ef3fca7b70d6
Andrew Thiele <308butterfly@gmail.com>
2021-03-17 03:11:32 -0500

    Create Game Integration Test passed

--- TriviaServer/TriviaServer.Tests/Integration/GameRepositoryTest.cs
+++ TriviaServer/TriviaServer.Tests/Integration/GameRepositoryTest.cs
@@ -12,41 +12,45 @@ using Microsoft.EntityFrameworkCore;
 namespace TriviaServer.Tests.Integration
 {
 
-    public class GameRepositoryTests
+  public class GameRepositoryTests
+  {
+    [Fact]
+    public async Task SearchGame_ByValidGameId_Success()
     {
-        [Fact]
-        public async Task SearchGame_ByValidGameId_Success()
-        {
-            //arrange
-            using var contextfactory = new TestTriviaGameContextFactory();
-            using TriviaRankContext context = contextfactory.CreateContext();
+      //arrange
+      using var contextfactory = new TestTriviaGameContextFactory();
+      using TriviaRankContext context = contextfactory.CreateContext();
 
-            var insertedGame = new Game
-            {
-                Id = 3,
-                GameName = "Good Times",
-                OwnerId = 2,
-                StartDate = DateTime.Now,
-                GameMode = true,
-                TotalQuestions = 10,
-                IsPublic = true
-            };
 
-            await context.Games.AddAsync(insertedGame);
-            context.SaveChanges();
-            var repo = new GameRepository(context);
+      var insertOwner = new Player
+      {
+        Username = "gameOwner1",
+        Password = "password",
+        Birthday = DateTime.Now,
+        Points = 100,
+        FirstName = "Test",
+        LastName = "Player"
 
-            //act
-            GameModel game = await repo.SearchGames(insertedGame.Id);
+      };
 
-            //assert
-            Assert.Equal(insertedGame.Id, game.Id);
-            Assert.Equal(insertedGame.GameName, game.GameName);
-            Assert.Equal(insertedGame.OwnerId, game.OwnerId);
-            Assert.Equal(insertedGame.StartDate, game.StartDate);
-            Assert.Equal(insertedGame.GameMode, game.GameMode);
-            Assert.Equal(insertedGame.TotalQuestions, game.TotalQuestions);
-            Assert.Equal(insertedGame.IsPublic, game.IsPublic);
-        }
+      await context.Players.AddAsync(insertOwner);
+      context.SaveChanges();
+
+      var repo = new GameRepository(context);
+      var insertedGame = await repo.CreateGame(insertOwner.Id, "some game", 10, true);
+
+      //act
+      var game = (await repo.SearchGames(1));
+
+      //assert
+      Assert.Equal(insertedGame.Id, game.Id);
+      Assert.Equal(insertedGame.GameName, game.GameName);
+      Assert.Equal(insertedGame.OwnerId, game.OwnerId);
+      Assert.Equal(insertedGame.StartDate, game.StartDate);
+      Assert.Equal(insertedGame.EndDate, game.EndDate);
+      Assert.Equal(insertedGame.GameMode, game.GameMode);
+      Assert.Equal(insertedGame.TotalQuestions, game.TotalQuestions);
+      Assert.Equal(insertedGame.IsPublic, game.IsPublic);
     }
+  }
 }
