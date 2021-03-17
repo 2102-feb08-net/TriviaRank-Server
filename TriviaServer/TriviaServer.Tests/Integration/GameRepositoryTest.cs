be05cbc173bf4be6826da044269884f8e63e9291
Josh Weaver <jweaver86@aim.com>
2021-03-16 23:37:31 -0400

    finished integration tests for Games

--- TriviaServer/TriviaServer.Tests/Integration/GameRepositoryTest.cs
+++ TriviaServer/TriviaServer.Tests/Integration/GameRepositoryTest.cs
@@ -15,76 +15,7 @@ namespace TriviaServer.Tests.Integration
     public class GameRepositoryTests
     {
         [Fact]
-        public async Task CheckValidGameId_Success()
-        {
-            //arrange
-            using var contextfactory = new TestTriviaGameContextFactory();
-            using TriviaRankContext context = contextfactory.CreateContext();
-
-            var insertedGame = new Game
-            {
-                Id = 3,
-                GameName = "Good Times",
-                OwnerId = 2,
-                StartDate = DateTime.Now,
-                GameMode = true,
-                TotalQuestions = 10,
-                IsPublic = true
-            };
-
-            await context.Games.AddAsync(insertedGame);
-            context.SaveChanges();
-            var repo = new GameRepository(context);
-
-            //act
-            GameModel game = await repo.SearchGames(insertedGame.Id);
-
-            //assert
-            Assert.Equal(insertedGame.Id, game.Id);
-            Assert.Equal(insertedGame.GameName, game.GameName);
-            Assert.Equal(insertedGame.OwnerId, game.OwnerId);
-            Assert.Equal(insertedGame.StartDate, game.StartDate);
-            Assert.Equal(insertedGame.GameMode, game.GameMode);
-            Assert.Equal(insertedGame.TotalQuestions, game.TotalQuestions);
-            Assert.Equal(insertedGame.IsPublic, game.IsPublic);
-        }
-        [Fact]
-        public async Task CheckValidGameId_Fail()
-        {
-            //arrange
-            using var contextfactory = new TestTriviaGameContextFactory();
-            using TriviaRankContext context = contextfactory.CreateContext();
-
-            var insertedGame = new Game
-            {
-                Id = 3,
-                GameName = "Good Times",
-                OwnerId = 2,
-                StartDate = DateTime.Now,
-                GameMode = true,
-                TotalQuestions = 10,
-                IsPublic = true
-            };
-
-            await context.Games.AddAsync(insertedGame);
-            context.SaveChanges();
-            var repo = new GameRepository(context);
-
-            //act
-            GameModel game = await repo.SearchGames(insertedGame.Id);
-
-            //assert
-            Assert.Equal(insertedGame.Id +1, game.Id);
-            Assert.Equal(insertedGame.GameName, game.GameName);
-            Assert.Equal(insertedGame.OwnerId, game.OwnerId);
-            Assert.Equal(insertedGame.StartDate, game.StartDate);
-            Assert.Equal(insertedGame.GameMode, game.GameMode);
-            Assert.Equal(insertedGame.TotalQuestions, game.TotalQuestions);
-            Assert.Equal(insertedGame.IsPublic, game.IsPublic);
-        }
-
-        [Fact]
-        public async Task CreateGame_Success()
+        public async Task SearchGame_ByValidGameId_Success()
         {
             //arrange
             using var contextfactory = new TestTriviaGameContextFactory();
