9f0e9ffb4df02554b4647ce2ec47202a314ed244
Andrew Thiele <308butterfly@gmail.com>
2021-03-18 00:11:14 -0500

    added some serverside validation to Player Model class

--- TriviaServer/TriviaServer.Models/PlayerModel.cs
+++ TriviaServer/TriviaServer.Models/PlayerModel.cs
@@ -11,13 +11,114 @@ namespace TriviaServer.Models
   public class PlayerModel
   {
     const int NAME_LIMIT = 50;
+    private int _id;
+    private string _userName;
+    private string _password;
+    private string _firstName;
+    private string _lastName;
+    private DateTimeOffset _birthday;
+    private int _points;
 
-    public string Username { get; set; }
-    public int Id { get; set; }
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public DateTimeOffset Birthday { get; set; }
-    public int Points { get; set; }
+    public PlayerModel() { }
+
+    public PlayerModel(int id, string userName, string password, string firstName, string lastName, DateTimeOffset birthday, int points)
+    {
+      this._id = id;
+      this._userName = userName;
+      this._password = password;
+      this._firstName = firstName;
+      this._lastName = lastName;
+      this._birthday = birthday;
+      this._points = points;
+    }
+
+    public string Username
+    {
+      get => _userName;
+      set
+      {
+        if (StringIsOnlyAlphaNumberic(value))
+        {
+          _userName = value;
+        }
+        else
+        {
+          throw new InvalidOperationException("Name can only contain alphabetic letters.");
+        }
+      }
+    }
+
+    public string Password
+    {
+      get => _password;
+      set => _password = value;
+    }
+    public int Id
+    {
+      get => _id;
+      set
+      {
+        if (value > 0)
+        {
+          _id = value;
+        }
+        else
+        {
+          throw new InvalidOperationException("Id must be greater than zero");
+        }
+      }
+    }
+    public string FirstName
+    {
+      get => _firstName;
+      set
+      {
+        if (StringIsValidName(value))
+        {
+          _firstName = value;
+        }
+        else
+        {
+          throw new InvalidOperationException("Invalid first Name");
+        }
+      }
+    }
+    public string LastName
+    {
+      get => _lastName;
+      set
+      {
+        if (StringIsValidName(value))
+        {
+          _lastName = value;
+        }
+        else
+        {
+          throw new InvalidOperationException("Invalid last Name");
+        }
+      }
+    }
+    public DateTimeOffset Birthday
+    {
+      get => _birthday;
+      set => _birthday = value;
+    }
+    public int Points
+    {
+      get => _points;
+      set
+      {
+        // What to do about the MAXINT
+        if (value > -1)
+        {
+          _points = value;
+        }
+        else
+        {
+          throw new InvalidOperationException("Points must be greater than zero");
+        }
+      }
+    }
 
     /// <summary>
     /// Returns true is string is alphanumberic and is one character or longer
@@ -26,10 +127,12 @@ namespace TriviaServer.Models
     /// <returns></returns>
     public bool StringIsOnlyAlphaNumberic(string str)
     {
+      // length of username must be > 0 and < 51
       if (str.Length == 0 || str.Length > NAME_LIMIT)
       {
         return false;
       }
+
 
       if (!Regex.IsMatch(str, "^[a-zA-Z0-9]*$"))
       {
