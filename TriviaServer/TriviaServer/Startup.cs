dddc7857b547dca5e5dddb1c6bdab3c355b78796
Andrew Park <modem428@gmail.com>
2021-03-19 17:26:51 -0700

    update cors

--- TriviaServer/TriviaServer/Startup.cs
+++ TriviaServer/TriviaServer/Startup.cs
@@ -35,7 +35,7 @@ namespace TriviaServer
                 options.AddDefaultPolicy(
                     builder =>
                     {
-                        builder.WithOrigins("http://localhost:4200");
+                        builder.WithOrigins("http://localhost:4200").AllowAnyMethod();
                     });
             });
 
