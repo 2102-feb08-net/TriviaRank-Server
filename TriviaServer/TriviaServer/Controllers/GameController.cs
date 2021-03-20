31e6364a94b29bda0fe4e082acc583cd8e889ea4
Andrew Park <modem428@gmail.com>
2021-03-20 15:26:13 -0700

    add route to add player to game

--- TriviaServer/TriviaServer/Controllers/GameController.cs
+++ TriviaServer/TriviaServer/Controllers/GameController.cs
@@ -9,40 +9,40 @@ using TriviaServer.Models.Repositories;
 
 namespace TriviaServer.Controllers
 {
-  [ApiController]
-  public class GameContoller : ControllerBase
-  {
-    private readonly IGameRepository _gameRepo;
-
-    public GameContoller(IGameRepository gameRepo)
+    [ApiController]
+    public class GameContoller : ControllerBase
     {
-      _gameRepo = gameRepo;
-    }
+        private readonly IGameRepository _gameRepo;
 
-    [HttpGet("/api/games")]
-    public async Task<IActionResult> GetAllGames()
-    {
-      List<GameModel> dbGames = await _gameRepo.GetAllGames();
+        public GameContoller(IGameRepository gameRepo)
+        {
+            _gameRepo = gameRepo;
+        }
 
-      return Ok(dbGames);
-    }
+        [HttpGet("/api/games")]
+        public async Task<IActionResult> GetAllGames()
+        {
+            List<GameModel> dbGames = await _gameRepo.GetAllGames();
+
+            return Ok(dbGames);
+        }
 
 
-    [HttpGet("api/game/{id}")]
-    public async Task<IActionResult> GetGameById(int id)
-    {
-      GameModel game;
-      try
-      {
-        game = await _gameRepo.SearchGames(id);
-      }
+        [HttpGet("api/game/{id}")]
+        public async Task<IActionResult> GetGameById(int id)
+        {
+            GameModel game;
+            try
+            {
+                game = await _gameRepo.SearchGames(id);
+            }
 
-      catch (Exception)
-      {
-        return StatusCode(500);
-      }
-      return Ok(game);
-    }
+            catch (Exception)
+            {
+                return StatusCode(500);
+            }
+            return Ok(game);
+        }
 
         [HttpPost("api/game/{gameId}/player")]
         public async Task<IActionResult> AddPlayerToGame(int gameId, PlayerModel player)
